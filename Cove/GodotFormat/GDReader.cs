c1d0d09b5b6ba703d1303e51ec86836fd9e6d3f6
Your Name <50252724+DrMeepso@users.noreply.github.com>
2024-11-28 17:32:19 +1300

    Add Apache-2.0 license to files

--- Cove/GodotFormat/GDReader.cs
+++ Cove/GodotFormat/GDReader.cs
@@ -1,5 +1,18 @@
-﻿// This read Godot's serialized format
-// https://docs.godotengine.org/en/stable/tutorials/io/binary_serialization_api.html
+﻿/*
+   Copyright 2024 DrMeepso
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
 
 namespace Cove.GodotFormat
 {
